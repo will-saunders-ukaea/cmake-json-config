e51ec230b20496dcb4c13a46bd310300f1d59ef7
Will Saunders <will.saunders@ukaea.uk>
2024-11-21 17:34:45 +0000

    cleanup of testing harness

--- tests/test_json_parse.py
+++ tests/test_json_parse.py
@@ -1,11 +1,15 @@
 import pytest
 import json
+import shutil
 
 from test_resources.testing_harness import *
 
+
+def test_cmake_exists():
+    assert shutil.which("cmake") is not None
+
+
 def test_pair_0():
     cmake_run = CMakeRun(*get_test_paths("pair_0"))
+    cmake_run.verbose = True
     assert cmake_run()
-
-
-
--- tests/test_resources/testing_harness.py
+++ tests/test_resources/testing_harness.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import shutil
 
+VERBOSE = False
 JSONCONFIG = "JSONConfig.cmake"
 TEST_RESOURCES_PATH = os.path.expanduser(
     os.path.dirname(os.path.abspath(__file__))
@@ -30,7 +31,16 @@ def get_test_paths(path: str) -> tuple[str, str]:
 
 def setup_test_directory(path: str, json_default: str, json_spec: str) -> None:
     """
-    Setup a directory to run the test in.
+    Setup a directory to run the test with the structure:
+
+    ├── build
+    ├── cmake
+    │   └── cmake-json-config
+    │       └── JSONConfig.cmake
+    ├── CMakeLists.txt
+    └── config
+        ├── default.json
+        └── spec.json
 
     :param str path: Directory to setup test in.
     :param str json_default: JSON file for default config.
@@ -68,14 +78,21 @@ class CMakeRun:
     def __init__(self, json_default: str, json_spec: str):
         self.json_default = json_default
         self.json_spec = json_spec
+        self.verbose = VERBOSE
+        self.tree_exists = shutil.which("tree") is not None
+        self.cmd = ["cmake", ".."]
+
+    def _call_tree(self, tmp_dir):
+        if self.tree_exists:
+            subprocess.check_call(["tree"], cwd=tmp_dir)
 
     def __call__(self) -> bool:
         tmp_dir_handle = tempfile.TemporaryDirectory()
         tmp_dir = tmp_dir_handle.name
         setup_test_directory(tmp_dir, self.json_default, self.json_spec)
-        subprocess.check_call(["tree"], cwd=tmp_dir)
 
-        cmd = ["cmake", ".."]
+        if self.verbose:
+            self._call_tree(tmp_dir)
 
         stdout_filename = os.path.join(tmp_dir, "cmake.stdout")
         stderr_filename = os.path.join(tmp_dir, "cmake.stderr")
@@ -83,7 +100,7 @@ class CMakeRun:
             with open(stderr_filename, "w") as stderr_fh:
                 try:
                     subprocess.check_call(
-                        cmd,
+                        self.cmd,
                         cwd=os.path.join(tmp_dir, "build"),
                         stdout=stdout_fh,
                         stderr=stderr_fh,
@@ -92,17 +109,15 @@ class CMakeRun:
                     print(e)
                     return False
 
-        print("")
-        print("-" * 80)
-        with open(stdout_filename) as fh:
-            print(fh.read())
-
-        print("." * 80)
-        with open(stderr_filename) as fh:
-            print(fh.read())
-
-        print("-" * 80)
-
-        subprocess.check_call(["tree"], cwd=tmp_dir)
+        if self.verbose:
+            print("")
+            print("-" * 80)
+            with open(stdout_filename) as fh:
+                print(fh.read())
+            print("." * 80)
+            with open(stderr_filename) as fh:
+                print(fh.read())
+            print("-" * 80)
+            self._call_tree(tmp_dir)
 
         return True
